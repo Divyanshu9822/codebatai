619a226ee8b38d9e3dff96e91697f5cdfa7a91c6
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-06-03 13:13:26 +0530

    added edited event listner on issues

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
-import { handleIssueOpened } from "./handlers/issueHandler.js";
-import { handlePullRequestEvents } from "./handlers/pullRequesthandler.js";
+import { handleIssueOpened } from './handlers/issueHandler.js';
+import { handlePullRequestEvents } from './handlers/pullRequesthandler.js';
 
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
 export default (app) => {
-  app.log.info("Yay, the app was loaded!");
+  app.log.info('Yay, the app was loaded!');
 
-  app.on("issues.opened", handleIssueOpened);
+  app.on(['issues.opened', 'issues.edited'], handleIssueOpened);
 
-  app.on(
-    [
-      "pull_request.opened",
-      "pull_request.reopened",
-      "pull_request.synchronize",
-    ],
-    handlePullRequestEvents
-  );
+  app.on(['pull_request.opened', 'pull_request.reopened', 'pull_request.synchronize'], handlePullRequestEvents);
 };
